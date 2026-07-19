e36f85f67fc12b9a7c6876fcb286d68715e2a134
devleloperravisharma <47600036+devleloperravisharma@users.noreply.github.com>
2026-07-19 12:59:51 -0500

    Add files via upload

--- stacks.py
+++ stacks.py
@@ -6,5 +6,18 @@ class Stack:
     def spaces_left_in_stack(self):
         spaces_left = self.limit - len(self.list)
         return spaces_left
+    def stack_empty(self):
+        if self.spaces_left_in_stack() == 0:
+            return True
+        else:
+            return False
+    def add_more(self):
+        if self.spaces_left_in_stack() < self.limit:
+            ask = print(input("add a value."))
+            self.list.append(ask)
+            print(self.list)
+        else:
+            print("there's no space left in the stack. please delete some elements necessary")
+            
 # positional argument - expecting something in function, where you have to give value. "20" is a positional argument for the limit. all dependent on parameter
 object = Stack(20)
